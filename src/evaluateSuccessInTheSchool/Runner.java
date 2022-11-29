a234dad3ad7ee5e37412cf33f353c3c84dda9765
İhsan Algül <algulihsan@hotmail.com>
2022-11-29 22:10:35 +0100

    evaluateSuccessInTheSchool first commit

---
+++ src/evaluateSuccessInTheSchool/Runner.java
@@ -0,0 +1,38 @@
+package evaluateSuccessInTheSchool;
+
+import java.util.Scanner;
+
+public class Runner {
+    /*
+    Dersler : Matematik, Fizik, Türkçe, Kimya, Müzik
+
+    Geçme Notu : 55
+
+    Ödev
+    Eğer girilen ders notları 0 veya 100 arasında değil ise ortalamaya katılmasın.
+     */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] lessons = {"Matematik", "Fizik", "Türkçe", "Kimya", "Müzik"};
+        int temp;
+        int average = 0;
+        int counter = 0;
+
+
+        for (String w : lessons) {
+            System.out.println("Please enter grade for "+w + " lesson ");
+            temp = scanner.nextInt();
+            if (temp < 100 && temp > 0) {
+                ++counter;
+                average += temp;
+                average /= counter;
+            }
+        }
+
+        if (average >= 55) {
+            System.out.println("The student pass the class successfully!\nAverage = " + average);
+        } else System.out.println("The student failed! Average: "+average+"\nAverage must be 55 to pass in minimum.");
+
+    }
+}
