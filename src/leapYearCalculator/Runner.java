00371440ae6406a7c7c7a4a8573cd4f6e0569f4d
IHSAN ALGUL <algulihsan@hotmail.com>
2022-12-04 21:03:12 +0000

    leap year calculator first commit

--- src/leapYearCalculator/Runner.java
+++ src/leapYearCalculator/Runner.java
@@ -1,9 +1,27 @@
 package leapYearCalculator;
 
+import java.util.Scanner;
+
 /**
  * Runner
  */
 public class Runner {
 
-    main
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = 0;
+
+        System.out.println("Yıl Giriniz : ");
+        year = scanner.nextInt();
+
+        if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println(year + " bir artık yıldır !");
+        } else if (year % 100 != 0 && year % 4 == 0) {
+            System.out.println(year + " bir artık yıldır !");
+        } else {
+            System.out.println(year + " bir artık yıldır değildir !");
+
+        }
+        scanner.close();
+    }
 }
