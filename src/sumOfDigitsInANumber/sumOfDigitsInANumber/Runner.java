1f0ab7a6ed8a6ed320aa2322f435a742ea653584
IHSAN ALGUL <algulihsan@hotmail.com>
2022-12-05 20:56:20 +0000

    sumOfDigitsInANumber first commit

---
+++ src/sumOfDigitsInANumber/sumOfDigitsInANumber/Runner.java
@@ -0,0 +1,25 @@
+package sumOfDigitsInANumber;
+
+/*
+Ödev
+Bir sayının basamak sayılarının toplamını hesaplayan program yazınız.
+
+Örnek : 1643 = 1 + 6 + 4 + 3 = 14
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        int number = 1643;
+        int result = 0, temp = 0;
+
+        while (number != 0) {
+            System.out.println(number);
+            temp = number % 10;
+            result += temp;
+            number /= 10;
+        }
+
+        System.out.println("Result:" + result);
+    }
+
+}
