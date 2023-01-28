372d491ee9873625cf034d1ca7ebc7b909a5a069
İhsan Algül <algulihsan@hotmail.com>
2023-01-28 13:47:18 +0100

    calc. recursive exponential

---
+++ src/recursiveMethods/Runner.java
@@ -0,0 +1,31 @@
+package recursiveMethods;
+
+import java.util.Scanner;
+
+public class Runner {
+    /*
+    Taban değeri giriniz :2
+    Üs değerini giriniz : 0
+    Sonuç : 1
+     */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter base: ");
+        int base = scanner.nextInt();
+        System.out.print("Enter exp: ");
+        int exp = scanner.nextInt();
+        int res = recursiveMethod(base, exp);
+        System.out.print("Result = " + res);
+    }
+
+    private static int recursiveMethod(int base, int exp) {
+        if (exp == 0) {
+            return 1;
+        } else {
+            exp--;
+            return base * recursiveMethod(base, exp);
+        }
+    }
+}
